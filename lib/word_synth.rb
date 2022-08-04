304b81387c4fb3f6d337cd23373d797891625784
川島慎悟 <kawashingo1990@gmail.com>
2022-08-04 22:00:12 +0900

    section10 例題

---
+++ lib/word_synth.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+class WordSynth
+  def initialize
+    @effects = []
+  end
+
+  def add_effect(effect)
+    @effects << effect
+  end
+
+  def play(original_words)
+    words = original_words
+    @effects.each do |effect|
+      words = effect.call(words)
+    end
+    words
+  end
+end
